5e6588d97fa821b673c8c01b0a29083bf8978d8a
Aziz Vohra <azizvohra@Azizs-MacBook-Air.local>
2022-07-05 15:55:49 +0530

    Final Commit

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 // get all workouts
 const getWorkouts = async (req, res) => {
   const workouts = await Workout.find({}).sort({createdAt: -1})
+
   res.status(200).json(workouts)
 }
 
@@ -20,12 +21,28 @@ const getWorkout = async (req, res) => {
   if (!workout) {
     return res.status(404).json({error: 'No such workout'})
   }
+
   res.status(200).json(workout)
 }
 
 // create a new workout
 const createWorkout = async (req, res) => {
   const {title, load, reps} = req.body
+
+  let emptyFields = []
+
+  if (!title) {
+    emptyFields.push('title')
+  }
+  if (!load) {
+    emptyFields.push('load')
+  }
+  if (!reps) {
+    emptyFields.push('reps')
+  }
+  if (emptyFields.length > 0) {
+    return res.status(400).json({ error: 'Please fill in all fields', emptyFields })
+  }
 
   // add to the database
   try {
